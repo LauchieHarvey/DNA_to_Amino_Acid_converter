aafaac597431b61a8e351d872a87c276d0c6b0e1
LauchieHarvey <lauchieharvey@gmail.com>
2019-12-30 13:38:27 +1000

    Memory allocation and made DNA_to_RNA a separate function.

--- geneSequenceReader.c
+++ geneSequenceReader.c
@@ -2,21 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
-	if (argc != 2)
-	{
-		printf("Please enter the sequence as a command line argument.\n");
-		return 1;
-	}
-	char *DNA_sequence = malloc(strlen(argv[1]) * sizeof(char) + 1);
-	strcpy(DNA_sequence, argv[1]);
-
-	printf("%s \n", DNA_sequence);
-
-	char *RNA_sequence = malloc(strlen(argv[1]) * sizeof(char) + 1);
+char* DNA_to_RNA(char* DNA_sequence){
+	char *RNA_sequence = malloc(strlen(DNA_sequence) * sizeof(char) + 1);
 	strcpy(RNA_sequence, DNA_sequence);
-
-
+	if (!RNA_sequence)
+	{
+		exit(1);
+	}
 	int n = strlen(DNA_sequence);
 	for (int i = 0; i < n; i++)
 	{
@@ -25,7 +17,28 @@ int main(int argc, char* argv[]){
 			RNA_sequence[i] = 'U';
 		}
 	}
-	printf("%s \n", RNA_sequence);
+	return RNA_sequence;
+}
+
+int main(int argc, char* argv[]){
+	if (argc != 2)
+	{
+		printf("Please enter the sequence as a command line argument.\n");
+		return 1;
+	}
+	char *DNA_sequence = malloc(strlen(argv[1]) * sizeof(char) + 1);
+	if (!DNA_sequence)
+	{
+		return 1;
+	}
+	strcpy(DNA_sequence, argv[1]);
+
+	printf("DNA: %s \n", DNA_sequence);
+
+	char *RNA_sequence = malloc(strlen(argv[1]) * sizeof(char) + 1);
+	strcpy(RNA_sequence, DNA_to_RNA(DNA_sequence));
+
+	printf("RNA: %s \n", RNA_sequence);
 
 	free(DNA_sequence);
 	free(RNA_sequence);
