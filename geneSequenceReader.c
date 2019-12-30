574bfb63eaa1dc6ef1f366f46b130164443c548e
LauchieHarvey <lauchieharvey@gmail.com>
2019-12-30 11:10:43 +1100

    Added command line args for geneSequenceReader.c

--- geneSequenceReader.c
+++ geneSequenceReader.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void){
-	printf("Hello World!");
-	
+int main(int argc, char* argv[]){
+	char *fullSequence = malloc(strlen(argv[1]) * sizeof(char) + 1);
+
+	strcpy(fullSequence, argv[1]);
+
+	printf("%s \n", fullSequence);
+
+	free(fullSequence);
 }
