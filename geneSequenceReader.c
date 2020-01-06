c99f5f249e8734e4b6c3d8091f39261454036f5c
LauchieHarvey <lauchieharvey@gmail.com>
2020-01-06 18:07:29 +1000

    Implemented 3D array algorithm for converting to amino acids from RNA.

--- geneSequenceReader.c
+++ geneSequenceReader.c
@@ -5,70 +5,60 @@
 char* DNA_to_RNA(int lengthOfSequence, char* DNA_sequence){
 	char *RNA_sequence = malloc(strlen(DNA_sequence) * sizeof(char) + 1);
 	strcpy(RNA_sequence, DNA_sequence);
-	if (!RNA_sequence)
-	{
-		exit(1);
-	}
+	if (!RNA_sequence) {exit(1);}
+
 	for (int i = 0; i < lengthOfSequence; i++)
 	{
-		if (RNA_sequence[i] == 'T')
-		{
-			RNA_sequence[i] = 'U';
-		}
+		if(RNA_sequence[i] == 'T') {RNA_sequence[i] = 'U';}
 	}
 	return RNA_sequence;
 }
 
-char *RNA_to_Amino_Acid(int lengthOfSequence, char * RNA_sequence){
-	char *aminoAcidSequence = malloc(lengthOfSequence * sizeof(char) + 1);
+char *returnAminoAcid(int firstNucleotideIndex, int secondNucletideIndex, int thirdNucleotideIndex){
+	char *aminoAcidArray[4][4][4] = {
+		{{"Phe", "Phe", "Leu", "Leu"}, {"Ser", "Ser", "Ser", "Ser"}, {"Tyr", "Tyr", "Stop", "Stop"}, {"Cys", "Cys", "Stop", "Trp"}},
+		{{"Leu", "Leu", "Leu", "Leu"}, {"Pro", "Pro", "Pro", "Pro"}, {"His", "His", "Gln", "Gln"}, {"Arg", "Arg", "Arg", "Arg"}},
+		{{"Ile", "Ile", "Ile", "Met"}, {"Thr", "Thr", "Thr", "Thr"}, {"Asn", "Asn", "Lys", "Lys"}, {"Ser", "Ser", "Arg", "Arg"}},
+		{{"Val", "Val", "Val", "Val"}, {"Ala", "Ala", "Ala", "Ala"}, {"Asp", "Asp", "Glu", "Glu"}, {"Gly", "Gly", "Gly", "Gly"}},
+	};
+
+	return aminoAcidArray[firstNucleotideIndex][secondNucletideIndex][thirdNucleotideIndex];
+}
+
+int *RNA_to_RNA_integer_Sequence(int lengthOfSequence, char *RNA_sequence){
+	int *RNA_integer_Sequence = malloc(lengthOfSequence * sizeof(int));
+	if(!RNA_integer_Sequence) {exit(1);}
+
+	for (int i = 0; i < lengthOfSequence; i++)
+	{
+		if (RNA_sequence[i] == 'U')
+		{
+			RNA_integer_Sequence[i] = 0;
+		}else if (RNA_sequence[i] == 'C')
+		{
+			RNA_integer_Sequence[i] = 1;
+		}else if (RNA_sequence[i] == 'A')
+		{
+			RNA_integer_Sequence[i] = 2;
+		}else if (RNA_sequence[i] == 'G')
+		{
+			RNA_integer_Sequence[i] = 3;
+		}
+	}
+	return RNA_integer_Sequence;
+}
+
+void RNA_to_Amino_Acid(int lengthOfSequence, char * RNA_sequence){
 	FILE *fp = fopen("aminoAcidSequence.txt", "w");
-	strcpy(aminoAcidSequence, RNA_sequence);
-	
+
+	int *RNA_integer_Sequence = RNA_to_RNA_integer_Sequence(lengthOfSequence, RNA_sequence);
+
 	for (int i = 0; i < lengthOfSequence; i += 3)
 	{
-		if (aminoAcidSequence[i] == 'U')
-		{
-			if (aminoAcidSequence[i + 1] == 'U')
-			{
-				if (aminoAcidSequence[i + 2] == 'U' || aminoAcidSequence[i + 2] == 'C')
-				{
-					fprintf(fp, "Phe ");
-				}
-				else fprintf(fp, "Leu ");	
-			}
-			else if (aminoAcidSequence[i + 1] == 'C')
-			{
-				fprintf(fp, "Ser ");
-			}		
-			else if (aminoAcidSequence[i + 1] == 'A')
-			{
-				if (aminoAcidSequence[i + 2] == 'U' || aminoAcidSequence[i + 2] == 'C')
-				{
-					fprintf(fp, "Tyr ");
-				}
-				else fprintf(fp, "STOP");
-			}
-			else if (aminoAcidSequence[i + 1] == 'G')
-			{
-				if (aminoAcidSequence[i + 2] == 'U' || aminoAcidSequence[i + 2] == 'C')
-				{
-					fprintf(fp, "Cys ");
-				}
-				else if (aminoAcidSequence[i + 2] == 'A')
-				{
-					fprintf(fp, "STOP");
-				}
-				else if (aminoAcidSequence[i + 2] == 'G')
-				{
-					fprintf(fp, "Trp ");
-				}
-			}			
-		}
+		fprintf(fp, returnAminoAcid(RNA_integer_Sequence[i], RNA_integer_Sequence[i + 1], RNA_integer_Sequence[i + 2]));
 	}
 
 	fclose(fp);
-	strcpy(aminoAcidSequence, "hello");
-	return aminoAcidSequence;
 }
 
 int main(int argc, char* argv[]){
@@ -79,10 +69,7 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 	char *DNA_sequence = malloc(lengthOfSequence * sizeof(char) + 1);
-	if (!DNA_sequence)
-	{
-		return 1;
-	}
+	if (!DNA_sequence) {return 1;}
 	strcpy(DNA_sequence, argv[1]);
 
 	printf("DNA: %s \n", DNA_sequence);
@@ -92,11 +79,12 @@ int main(int argc, char* argv[]){
 
 	printf("RNA: %s \n", RNA_sequence);
 
-	char *aminoAcidSequence = malloc(lengthOfSequence * sizeof(char) + 1); // If you want to add spaces between amino acids update this
-	strcpy(aminoAcidSequence, RNA_to_Amino_Acid(lengthOfSequence, RNA_sequence));
-	
-	printf("Amino Acids: %s \n", aminoAcidSequence);
+	//char *aminoAcidSequence = malloc(lengthOfSequence * sizeof(char) + 1); 
+	//variable not needed as of now, will be needed if you read the file in future
+	//	free(aminoAcidSequence); <-- add this in at the end also :)
+
+	RNA_to_Amino_Acid(lengthOfSequence, RNA_sequence);
+
 	free(DNA_sequence);
-	free(RNA_sequence);
-	free(aminoAcidSequence);
+	free(RNA_sequence);	
 }
